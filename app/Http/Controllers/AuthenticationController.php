76ed103aeab64c1896505908bef6070f173b1725
Bolo Chen <bolosstuff12@gmail.com>
2023-04-06 11:04:58 +0700

    fix validator

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -18,7 +18,7 @@ class AuthenticationController extends Controller
         ]);
 
         $user = User::where('email', $request->email)->first();
- 
+
         if (! $user || ! Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages([
                 'email' => ['The provided credentials are incorrect.'],
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use Illuminate\Validation\ValidationException;
 use App\Http\Controllers\AuthenticationController;
 
 // /*
@@ -27,3 +30,20 @@ Route::get('/posts/{id}', [PostController::class, 'show'])->middleware(['auth:sa
 Route::post('/login', [AuthenticationController::class, 'login']);
 Route::get('/logout', [AuthenticationController::class, 'logout'])->middleware(['auth:sanctum']);
 Route::get('/me', [AuthenticationController::class, 'me'])->middleware(['auth:sanctum']);
+
+// Route::post('/sanctum/token', function (Request $request) {
+//     $request->validate([
+//         'email' => 'required|email',
+//         'password' => 'required',
+//     ]);
+ 
+//     $user = User::where('email', $request->email)->first();
+ 
+//     if (! $user || ! Hash::check($request->password, $user->password)) {
+//         throw ValidationException::withMessages([
+//             'email' => ['The provided credentials are incorrect.'],
+//         ]);
+//     }
+ 
+//     return $user->createToken('login')->plainTextToken;
+// });
