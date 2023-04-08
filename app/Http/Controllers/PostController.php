039dfb55dbfaf580727a2ea746e7aebd11db1c02
Bolo Chen <bolosstuff12@gmail.com>
2023-04-08 13:16:10 +0700

    add image

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,7 @@ use App\Models\Post;
 use Illuminate\Http\Request;
 use App\Http\Resources\PostResource;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use App\Http\Resources\PostDetailResource;
 
 class PostController extends Controller
@@ -41,7 +42,18 @@ class PostController extends Controller
             'news_content' => 'required'
         ]);
 
+        $image = null;
+        if ($request->file) {
+
+            $fileName = $this->generateRandomString();
+            $extension = $request->file->extension();
+            $image = $fileName . '.' . $extension;
+
+            Storage::putFileAs('image', $request->file, $image);
+        }
+
         $validatedData['author'] = Auth::user()->id;
+        $validatedData['image'] = $image;
         $post = Post::create($validatedData);
 
         return new PostDetailResource($post->loadMissing('writer:id,username'));
@@ -66,5 +78,16 @@ class PostController extends Controller
         $post->delete();
 
         return new PostDetailResource($post->loadMissing('writer:id,username'));
+    }
+
+    function generateRandomString($length = 30)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[random_int(0, $charactersLength - 1)];
+        }
+        return $randomString;
     }
 }
