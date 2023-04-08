2e327c9a9a6f21d51024a2c951b10de2bdea5bba
Bolo Chen <bolosstuff12@gmail.com>
2023-04-08 11:48:54 +0700

    comment resource

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Comment extends Model
 {
@@ -12,4 +14,14 @@ class Comment extends Model
     protected $fillable = [
         'post_id', 'user_id', 'comments_content'
     ];
+
+    /**
+     * Get the commentator that owns the Comment
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function commentator(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
